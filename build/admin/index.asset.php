df71076787de20df65007ffd7ba1feed57f21103
ThemeHunk <themehunk@gmail.com>
2026-03-03 17:41:39 +0530

    build file

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '80825e9a65e3c432e201');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bcc4ac142462177e2fec');
