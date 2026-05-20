13239d1a04315e1fab3e839e644a3e9aa85067af
ThemeHunk <themehunk@gmail.com>
2026-05-20 15:56:21 +0530

    merge file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '56dc0ec6232bc7be76c6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a46d180a190dc0ab96c0');
