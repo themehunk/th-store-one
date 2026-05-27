04faabee7a9ce92d7c8c9ad7b4fa521732ca3521
Shivam Kushwah <37370154+devkushwahshivam@users.noreply.github.com>
2026-05-27 14:29:40 +0530

    backend style added

--- src/admin/components/GlobalSettings/icons.js
+++ src/admin/components/GlobalSettings/icons.js
@@ -20,6 +20,35 @@ export const ICONS = {
       />
     </svg>
   ),
+  UploadIcon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M12 15V5"
+        stroke="currentColor"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+      />
+      <path
+        d="M8.5 8.5L12 5L15.5 8.5"
+        stroke="currentColor"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5 18H19"
+        stroke="currentColor"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+      />
+    </svg>
+  ),
 
   USER: (
     <svg
