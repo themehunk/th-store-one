d45272d2d5e2476fb914ef060e5db93b724f49db
ThemeHunk <themehunk@gmail.com>
2026-07-01 14:12:56 +0530

    pro plugin activate

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '14bfa22fba059b169889');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2f978be605af2e0137ca');
