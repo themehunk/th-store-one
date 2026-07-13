4a3bdb0070a4c1c8a81232bbdcd78ac8a157220c
ThemeHunk <themehunk@gmail.com>
2026-07-13 12:15:32 +0530

    merg file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '85bfbd53c79c7b3728a1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9c292008c486b1812c22');
