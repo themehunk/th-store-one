6fce13c4bc1f07eb1c1f4c63bc3a0b2c65dc0f88
ThemeHunk <themehunk@gmail.com>
2026-07-17 13:35:47 +0530

    issue fixed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9c292008c486b1812c22');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '18bc7ec318e0758fe766');
