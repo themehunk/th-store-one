433e8003ac509a231bb95ff79fe4726a91ae3502
ThemeHunk <themehunk@gmail.com>
2026-08-18 12:01:15 +0530

    build file

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f5dfc9f43eb6875d4e2d');
+<?php return array('dependencies' => array(), 'version' => '0379e38bcd8b093ecbe7');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '612257c3bcf859da7617');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e932ac04a4780ec126f7');
