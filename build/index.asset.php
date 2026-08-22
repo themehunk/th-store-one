28c260f66dd82816b92ccefdc1b3430cc9312cfb
ThemeHunk <themehunk@gmail.com>
2026-08-22 11:10:08 +0530

    wishlist and cart merge file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c808ce412e10ddcdb934');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4405f052c113a5130fe7');
