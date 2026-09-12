f55199b37c6f52fad44563d5d1cb4293e052dc36
ThemeHunk <themehunk@gmail.com>
2026-09-12 13:48:51 +0530

    merge for wishlist issue resolved

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c8f615547e5228d74321');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'be6eb236b8fe3e6826ab');
